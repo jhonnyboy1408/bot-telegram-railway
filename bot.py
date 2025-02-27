1b4b6959c6a9f1685038876cc778421fb9c09dca
jhonnyboy1408 <83311402+jhonnyboy1408@users.noreply.github.com>
2025-02-27 17:00:34 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-"print('Bot rodando!')" 
+import time
+import requests
+
+TOKEN = 7720182921:AAERle2h8iUv_u5ToriU_VqWoDMqTK5lQtE
+CHAT_ID = 1122002906
+MENSAGEM = "Atualização do projeto: O desenvolvimento está em andamento."
+
+def enviar_mensagem():
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": MENSAGEM}
+    requests.post(url, data=data)
+
+
+while True:
+    enviar_mensagem()
+    time.sleep(3600)
